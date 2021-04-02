8c002c9203fca7c8fb44bce9f3b18170c3bb4298
dorariel1987 <64736310+dorariel1987@users.noreply.github.com>
2021-04-03 02:03:15 +0300

    Update producer.py

--- producer/producer.py
+++ producer/producer.py
@@ -1,5 +1,3 @@
-#!/usr/bin/python2
-
 import pika, logging, sys, argparse
 from argparse import RawTextHelpFormatter
 from time import sleep
@@ -9,10 +7,10 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(formatter_class=RawTextHelpFormatter,
                                  description='Run producer.py',
                                  epilog=examples)
-    parser.add_argument('-p', '--port', action='store', dest='port', help='The port to listen on.', required=False, default='5672')
-    parser.add_argument('-s', '--server', action='store', dest='server', help='The RabbitMQ server.', required=False, default='rabbitmq')
+    parser.add_argument('-p', '--port', action='store', dest='port', help='The port to listen on.')
+    parser.add_argument('-s', '--server', action='store', dest='server', help='The RabbitMQ server.')
     parser.add_argument('-m', '--message', action='store', dest='message', help='The message to send', required=False, default='Hello')
-    parser.add_argument('-r', '--repeat', action='store', dest='repeat', help='Number of times to repeat the message', required=False, default='1')
+    parser.add_argument('-r', '--repeat', action='store', dest='repeat', help='Number of times to repeat the message', required=False, default='30')
 
     args = parser.parse_args()
     if args.port == None:
@@ -27,7 +25,7 @@ if __name__ == '__main__':
 
     logging.basicConfig(level=logging.INFO)
     LOG = logging.getLogger(__name__)
-    credentials = pika.PlainCredentials('my_user', 'my_password')
+    credentials = pika.PlainCredentials('guest', 'guest')
     parameters = pika.ConnectionParameters(args.server,
                                            int(args.port),
                                            '/',
