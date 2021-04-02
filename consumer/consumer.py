9cf157e8a382f45fba9d524e7997f786181a6595
dorariel1987 <64736310+dorariel1987@users.noreply.github.com>
2021-04-03 01:53:16 +0300

    Update consumer.py

--- consumer/consumer.py
+++ consumer/consumer.py
@@ -1,17 +1,16 @@
 import pika, logging, sys, argparse, time
 from argparse import RawTextHelpFormatter
 from time import sleep
+import datetime, re
 
 def on_message(channel, method_frame, header_frame, body):
-    print method_frame.delivery_tag
-    print body
-    print
-    LOG.info('Message has been received %s', body)
+    date_now = datetime.datetime.now()
+    print(LOG.info(' Recieved Message : (%s) at %s' % (body, str(date_now))))
     channel.basic_ack(delivery_tag=method_frame.delivery_tag)
 
 
 if __name__ == '__main__':
-    examples = sys.argv[0] + " -p 5672 -s rabbitmq "
+    examples = sys.argv[0] + " -p 5672 -s rabbitmq"
     parser = argparse.ArgumentParser(formatter_class=RawTextHelpFormatter,
                                  description='Run consumer.py',
                                  epilog=examples)
@@ -27,7 +26,7 @@ if __name__ == '__main__':
         sys.exit(1)
 
     # sleep a few seconds to allow RabbitMQ server to come up
-    sleep(25)
+    sleep(5)
     logging.basicConfig(level=logging.INFO)
     LOG = logging.getLogger(__name__)
     credentials = pika.PlainCredentials('guest', 'guest')
@@ -38,10 +37,11 @@ if __name__ == '__main__':
     connection = pika.BlockingConnection(parameters)
     channel = connection.channel()
 
-    channel.queue_declare('pc')
+    channel.queue_declare('pc', durable=True)
     channel.basic_consume(on_message, 'pc')
 
     try:
+        # the  "start_consuming" method will trigger an infinite loop so that the consumer will wait for a message
         channel.start_consuming()
     except KeyboardInterrupt:
         channel.stop_consuming()
